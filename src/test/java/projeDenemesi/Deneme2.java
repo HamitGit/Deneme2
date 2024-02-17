1e843705251219dc6f924193bb23c9edefa3c156
Hamit1903 <williamakbal@gmail.com>
2024-02-17 15:51:42 -0800

    son deneme

--- src/test/java/projeDenemesi/Deneme2.java
+++ src/test/java/projeDenemesi/Deneme2.java
@@ -4,5 +4,6 @@ public class Deneme2 {
     public static void main(String[] args) {
         System.out.println("hahahah");
         System.out.println("ADKJAS;KDJ");
+        System.out.println("322");
     }
 }
