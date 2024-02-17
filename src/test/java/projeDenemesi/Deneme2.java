a8835b0a5a26fca2ceb4c01cd0fa0d652ac75607
Hamit1903 <williamakbal@gmail.com>
2024-02-17 15:50:33 -0800

    Bakalik pushlayacakmi

--- src/test/java/projeDenemesi/Deneme2.java
+++ src/test/java/projeDenemesi/Deneme2.java
@@ -3,5 +3,6 @@ package projeDenemesi;
 public class Deneme2 {
     public static void main(String[] args) {
         System.out.println("hahahah");
+        System.out.println("ADKJAS;KDJ");
     }
 }
